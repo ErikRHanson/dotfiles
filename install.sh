8002a56e90e9a16703d8ff6944e7b24f71f74661
Erik Hanson <Erik.Hanson@avellino.com>
2026-02-25 16:42:44 -0600

    update the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.config/nvim
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/init.lua ~/.config/nvim/init.lua
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
+ln -sf "$DOTFILES_DIR/init.lua" ~/.config/nvim/init.lua
 echo "Dotfiles linked!"
