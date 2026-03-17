26f79fee22eca762f4ea47a23679fe62e13eeccd
Erik Hanson <erik.hanson@transperfect.com>
2026-03-17 17:01:41 -0500

    create the exports.sh file and bin directory

---
+++ exports.sh
@@ -0,0 +1,12 @@
+# path to second_brain repo for notes
+export SECOND_BRAIN="$HOME/github.com/erikrhanson/second_brain"
+
+# Custom scripts 
+export PATH=$HOME/github.com/erikrhanson/dotfiles/bin:$PATH
+
+# Set Editor so system tools use Neovim
+export EDITOR="nvim"
+
+# alias Neovim
+alias v='nvim'
+
